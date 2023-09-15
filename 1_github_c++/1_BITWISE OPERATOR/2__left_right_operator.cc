690e448077e2915ac37bf99909ec3d8de0864e00
anshulnegi00 <78394338+anshulnegi00@users.noreply.github.com>
2023-09-15 21:09:16 +0530

    Add files via upload

---
+++ 1_github_c++/1_BITWISE OPERATOR/2__left_right_operator.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    
+    cout<<" a<<1 "<<(a<<1)<<endl;
+    cout<<" a<<2 "<<(a<<2)<<endl;
+    
+    cout<<" a>>1 "<<(a>>1)<<endl;
+    cout<<" a>>2 "<<(a>>2)<<endl;
+    
+}
